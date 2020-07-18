a27aca13de3832f2ff43cc2641547dcbba033971
edtorrez <etorrez2017@gmail.com>
2020-07-18 14:05:03 -0500

    arrow is closer to inside

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import Nav from './components/Nav.js';
-
 import Home from './pages/Home.js';
 
 import './css/App.css';
 import './css/Components.css';
 import './css/Cube.scss';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 import './css/index.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import App from './App';
 
 ReactDOM.render(
