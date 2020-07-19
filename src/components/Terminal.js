c7e7948493b955d0b1fde351f648d148bb432f62
edtorrez <etorrez2017@gmail.com>
2020-07-19 03:20:40 -0500

    fonts added

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -3,7 +3,9 @@ import React from 'react';
 function Terminal() {
   return(
     <div className='terminal'>
-
+      <div className='terminal-output'>
+        <h2> Hello World </h2>
+      </div>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return(
     <div className='home-page'>
-
+      <h1> Hello World </h1>
     </div>
   );
 }
