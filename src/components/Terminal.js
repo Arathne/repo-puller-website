1c7d4f6d9d34733a844c85116d783a8232a61261
edtorrez <etorrez2017@gmail.com>
2020-07-20 01:13:07 -0500

    moved startup terminal text to a json file

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -2,6 +2,7 @@
 /* the main challenge with this is that react is asynchronous */
 
 import React, {useState, useEffect, useRef} from 'react';
+import Startup from '../json/Startup.json';
 const sleep = (milliseconds) => {return new Promise(resolve => setTimeout(resolve, milliseconds))};
 
 function Terminal() {
@@ -28,53 +29,13 @@ function Terminal() {
   */
   useEffect( () => {
     async function effectAsync() {
-      await log("Award Modular BIOS v4.51PG, An Energy Star Ally");
-      await log("Copyright (C) 1984-95, Award Sofware, Inc");
-      await log("");
-      await log("#401A0-0207");
-      await log("");
-      await log("PENTIUM-MAX CPU at 200MHz");
-      await log("Memory Test : 65536X OK");
-      await log("");
-      await log("Initialize Plug and Play Cards...");
-      await log("PNP Init Completed");
-      await log("");
-      await log("Detecting Primary Master   ... MAXTOR 6L040J2");
-      await log("Detecting Primary Slave    ... ASUS CD-S520/A");
-      await log("Detecting Secondary Master ... TOSHIBA CD-ROM 6202D");
-      await log("Detecting Secondary Slave  ... TOSHIBA CD-ROM 6202D");
-      await log("CMOS Battery Failed");
-      await log("");
-      await log("Edgar Torrez");
-      await log("Potato aaskdkasd");
-      await log("Josh White");
-      await log("Edgar Torrez");
-      await log("Potato aaskdkasd");
-      await log("Josh White");
-      await log("Edgar Torrez");
-      await log("Potato aaskdkasd");
-      await log("Josh White");
-      await log("Edgar Torrez");
-      await log("Potato aaskdkasd");
-      await log("Josh White");
-      await log("Edgar Torrez");
-      await log("Potato aaskdkasd");
-      await log("Josh White");
-      await log("Edgar Torrez");
-      await log("Potato aaskdkasd");
-      await log("Josh White");
-      await log("Edgar Torrez");
-      await log("Potato aaskdkasd");
-      await log("Josh White");
-      await log("Edgar Torrez");
-      await log("Potato aaskdkasd");
-      await log("Josh White");
-      await log("Edgar Torrez");
-      await log("Potato aaskdkasd");
-      await log("Josh White");
-      await log("Edgar Torrez");
-      await log("Potato aaskdkasd");
-      await log("Josh White");
+      let data = [];
+      Startup.DOS.map( (line, index) => { // map is asyncronous and cannot use 'await log()'
+        data.push(line);
+      });
+
+      for (var i = 0; i < data.length; i++)
+        await log( data[i] );
     }
 
     effectAsync();
@@ -87,7 +48,7 @@ function Terminal() {
     return (
       <div>
         {logBuffer.map( (name, index) => {
-          return name !== '' ? (<h2 key={index}> {name} </h2>) : (<br></br>);
+          return name !== '' ? (<h2 key={index}> {name} </h2>) : (<br key={index}></br>);
         })}
       </div>
     );
@@ -100,7 +61,7 @@ function Terminal() {
     <div className='terminal'>
       <div className='terminal-output'>
         <LogDiv />
-        <h2 ref={bottomRef}> {currentLine} </h2>
+        <h2 ref={bottomRef} className='terminal-output-bottom'> {currentLine} </h2>
       </div>
     </div>
   );
