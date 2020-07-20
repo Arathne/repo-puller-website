7cc3ea68bcd0d24bb7257e64e33445dd18c71f69
edtorrez <etorrez2017@gmail.com>
2020-07-20 02:49:04 -0500

    changed scanline width

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 function Home() {
+  useEffect( () => {
+    console.log("START");
+    return () => {
+      console.log("END");
+   };
+  }, [])
+
   return(
     <div className='home-page'>
       <h1> Home </h1>
