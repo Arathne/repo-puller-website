4a4d1ba5ce80a13e6829834ba41b7d726fd0c1de
edtorrez <etorrez2017@gmail.com>
2020-07-31 15:54:23 -0500

    student component

--- src/components/Class.js
+++ src/components/Class.js
@@ -1,23 +1,9 @@
 import React, {useEffect, useState} from 'react';
+import Student from './Student.js'
 
-function Class(props) {
+function Class( props ) {
   const [className, setClassName] = useState('');
   const [students, setStudents] = useState([]);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log( event.target.firstName.value )
-    console.log( event.target.lastName.value )
-    console.log( event.target.userid.value )
-  }
-
-  const handleFocus = (event) => {
-    event.target.parentElement.className += 'active-student';
-  }
-
-  const handleBlur = (event) => {
-    event.target.parentElement.className = '';
-  }
 
   useEffect( () => {
     setClassName( props.json.class );
@@ -28,17 +14,13 @@ function Class(props) {
     <div>
       <h3> {className} </h3>
 
-      {/* iterates through each students and displays them */}
+      {/* iterates through each student and displays them */}
       {students.map( (student, s_index) => {
         return (
-          <form onSubmit={handleSubmit} key={`student-${s_index}`} className='student-form'>
-            <div>
-              <input type='text' onFocus={handleFocus} onBlur={handleBlur} defaultValue={student.firstName} name='firstName' className='text-field' />
-              <input type='text' onFocus={handleFocus} onBlur={handleBlur} defaultValue={student.lastName} name='lastName' className='text-field' />
-              <input type='text' onFocus={handleFocus} onBlur={handleBlur} defaultValue={student.userid} name='userid' className='text-field' />
-              <button type="submit"></button>
-            </div>
-          </form> )
+          <div key={`student-${s_index}`}>
+            <Student FirstName={student.firstName} LastName={student.lastName} UserID={student.userid} />
+          </div>
+        )
       })}
     </div>
   );
---
+++ src/components/Student.js
@@ -0,0 +1,49 @@
+import React, {useEffect, useState} from 'react';
+const Log = require('../modules/logger.js');
+
+function Student( props ) {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [userid, setUserid] = useState('');
+  const [css, setCss] = useState('');
+
+  useEffect( ()=>{
+    setFirstName( props.FirstName )
+    setLastName( props.LastName )
+    setUserid( props.UserID )
+  }, [])
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    let newFirstName = event.target.firstName.value;
+    let newLastName = event.target.lastName.value;
+    let newUserid = event.target.userid.value;
+
+    await Log.append('');
+    await Log.append(`OLD:  ${firstName}, ${lastName}, ${userid}`);
+    await Log.append(`NEW:  ${newFirstName}, ${newLastName}, ${newUserid}`);
+
+    /* send json */
+
+    /* if successful -- update */
+    setFirstName( newFirstName );
+    setLastName( newLastName );
+    setUserid( newUserid );
+  }
+
+  const handleFocus = (event) => { setCss('active-student'); }
+  const handleBlur = (event) => { setCss(''); }
+
+  return(
+    <form onSubmit={handleSubmit} className='student-form'>
+      <div className={css}>
+        <input type='text' onFocus={handleFocus} onBlur={handleBlur} defaultValue={firstName} name='firstName' className='text-field' />
+        <input type='text' onFocus={handleFocus} onBlur={handleBlur} defaultValue={lastName} name='lastName' className='text-field' />
+        <input type='text' onFocus={handleFocus} onBlur={handleBlur} defaultValue={userid} name='userid' className='text-field' />
+        <button type="submit"></button>
+      </div>
+    </form>
+  );
+}
+
+export default Student;
