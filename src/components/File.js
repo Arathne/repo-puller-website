ff306516f535beba3b38e1983ae6bbbe959c9dae
edtorrez <etorrez2017@gmail.com>
2020-08-06 21:34:24 -0500

    terminal message for downloads

--- src/components/File.js
+++ src/components/File.js
@@ -1,10 +1,12 @@
 import React, {useEffect, useState, useRef} from 'react';
 import {Redirect} from 'react-router';
 const Api = require('../modules/api.js');
+const Log = require('../modules/logger.js');
 
 function File(props) {
   const [name, setName] = useState('');
   const [url, setUrl] = useState('');
+  const [css, setCss] = useState('');
   const refTag = useRef(null);
 
   useEffect( () => {
@@ -12,15 +14,25 @@ function File(props) {
   }, [])
 
   const download = () => {
+    Log.append('starting download...', true);
+    Log.append('may take a min -- please do not leave page');
+    setCss('archive-download-active');
+
     Api.downloadFile(props.FileName).then( fileURL => {
       setUrl( fileURL );
-      refTag.current.click();
+      if( fileURL === '' ) {
+        Log.append('download failed', true)
+      } else {
+        refTag.current.click();
+        Log.append(`download complete - ${name}`, true);
+      }
+      setCss('');
     });
   }
 
   return(
     <div>
-      <button onClick={download}> {name} </button>
+      <button onClick={download} className={css}> {name} </button>
       <a href={url} ref={refTag} className='display-none' download={name}> {name} </a>)
     </div>
   );
